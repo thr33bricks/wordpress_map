103c56650fcbeb3c5270ffdefb15c8d72d78381f
thr33bricks <jormil1@abv.bg>
2022-03-31 12:53:10 +0300

    Added a script, which is executed once and it adds all major cities from every country in the world into the database

--- maps.php
+++ maps.php
@@ -77,296 +77,29 @@
             });
 
             <?php
-            $countries = array(
-                "AF",
-                "AL",
-                "DZ",
-                "AS",
-                "AD",
-                "AO",
-                "AI",
-                "AQ",
-                "AG",
-                "AR",
-                "AM",
-                "AW",
-                "AU",
-                "AT",
-                "AZ",
-                "BS",
-                "BH",
-                "BD",
-                "BB",
-                "BY",
-                "BE",
-                "BZ",
-                "BJ",
-                "BM",
-                "BT",
-                "BO",
-                "BQ",
-                "BA",
-                "BW",
-                "BV",
-                "BR",
-                "IO",
-                "BN",
-                "BG",
-                "BF",
-                "BI",
-                "CV",
-                "KH",
-                "CM",
-                "CA",
-                "KY",
-                "CF",
-                "TD",
-                "CL",
-                "CN",
-                "CX",
-                "CC",
-                "CO",
-                "KM",
-                "CD",
-                "CG",
-                "CK",
-                "CR",
-                "HR",
-                "CU",
-                "CW",
-                "CY",
-                "CZ",
-                "CI",
-                "DK",
-                "DJ",
-                "DM",
-                "DO",
-                "EC",
-                "EG",
-                "SV",
-                "GQ",
-                "ER",
-                "EE",
-                "SZ",
-                "ET",
-                "FK",
-                "FO",
-                "FJ",
-                "FI",
-                "FR",
-                "GF",
-                "PF",
-                "TF",
-                "GA",
-                "GM",
-                "GE",
-                "DE",
-                "GH",
-                "GI",
-                "GR",
-                "GL",
-                "GD",
-                "GP",
-                "GU",
-                "GT",
-                "GG",
-                "GN",
-                "GW",
-                "GY",
-                "HT",
-                "HM",
-                "VA",
-                "HN",
-                "HK",
-                "HU",
-                "IS",
-                "IN",
-                "ID",
-                "IR",
-                "IQ",
-                "IE",
-                "IM",
-                "IL",
-                "IT",
-                "JM",
-                "JP",
-                "JE",
-                "JO",
-                "KZ",
-                "KE",
-                "KI",
-                "KP",
-                "KR",
-                "KW",
-                "KG",
-                "LA",
-                "LV",
-                "LB",
-                "LS",
-                "LR",
-                "LY",
-                "LI",
-                "LT",
-                "LU",
-                "MO",
-                "MG",
-                "MW",
-                "MY",
-                "MV",
-                "ML",
-                "MT",
-                "MH",
-                "MQ",
-                "MR",
-                "MU",
-                "YT",
-                "MX",
-                "FM",
-                "MD",
-                "MC",
-                "MN",
-                "ME",
-                "MS",
-                "MA",
-                "MZ",
-                "MM",
-                "NA",
-                "NR",
-                "NP",
-                "NL",
-                "NC",
-                "NZ",
-                "NI",
-                "NE",
-                "NG",
-                "NU",
-                "NF",
-                "MP",
-                "NO",
-                "OM",
-                "PK",
-                "PW",
-                "PS",
-                "PA",
-                "PG",
-                "PY",
-                "PE",
-                "PH",
-                "PN",
-                "PL",
-                "PT",
-                "PR",
-                "QA",
-                "MK",
-                "RO",
-                "RU",
-                "RW",
-                "RE",
-                "BL",
-                "SH",
-                "KN",
-                "LC",
-                "MF",
-                "PM",
-                "VC",
-                "WS",
-                "SM",
-                "ST",
-                "SA",
-                "SN",
-                "RS",
-                "SC",
-                "SL",
-                "SG",
-                "SX",
-                "SK",
-                "SI",
-                "SB",
-                "SO",
-                "ZA",
-                "GS",
-                "SS",
-                "ES",
-                "LK",
-                "SD",
-                "SR",
-                "SJ",
-                "SE",
-                "CH",
-                "SY",
-                "TW",
-                "TJ",
-                "TZ",
-                "TH",
-                "TL",
-                "TG",
-                "TK",
-                "TO",
-                "TT",
-                "TN",
-                "TR",
-                "TM",
-                "TC",
-                "TV",
-                "UG",
-                "UA",
-                "AE",
-                "GB",
-                "UM",
-                "US",
-                "UY",
-                "UZ",
-                "VU",
-                "VE",
-                "VN",
-                "VG",
-                "VI",
-                "WF",
-                "EH",
-                "YE",
-                "ZM",
-                "ZW",
-                "AX"
-            );
-
-            $allCities = [];
-
-            //Get all top cities from all countries
-            foreach ($countries as $ctry) {
-                $curl = curl_init();
-
-                curl_setopt_array($curl, array(
-                    CURLOPT_URL => "https://api.apilayer.com/geo/country/cities/" . $ctry,
-                    CURLOPT_HTTPHEADER => array(
-                        "Content-Type: text/plain",
-                        "apikey: bTbrdZpP29yWNBJzPPYTiaUn1Gi9KWil"
-                    ),
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => "",
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 0,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => "GET"
-                ));
-
-                $response = curl_exec($curl);
-                curl_close($curl);
-
-                echo $response;
-                foreach ($response as $res) {
-                    $allCities[] = $res;
+            $servername = "localhost";
+            $username = "root";
+            $password = "MY_DB_PASS";
+            $dbname = "wpmap";
+            
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+              die("Connection failed: " . $conn->connect_error);
+            }
+            
+            $sql = "SELECT * FROM points";
+            $result = $conn->query($sql);
+            
+            if ($result->num_rows > 0) {
+                // output data of each row
+                // Display the content on the map
+                while($row = $result->fetch_assoc()) {
+                  echo ("addMarker(".$row["lat"].", ".$row["long"].", \"<b>".$row["name"]."</b>\");\n");
                 }
             }
-
-            //Display the content on the map
-            $path = preg_replace('/wp-content(?!.*wp-content).*/', '', __DIR__);
-            include($path . 'wp-load.php');
-            $mydb = new wpdb('root', 'nfs', 'wpmap', 'localhost');
-            $rows = $mydb->get_results("SELECT * FROM points");
-            foreach ($rows as $obj) {
-                echo ("addMarker($obj->lat, $obj->long, '<b>$obj->name</b>');\n");
-            }
+            $conn->close();
             ?>
 
             center = bounds.getCenter();
@@ -379,7 +112,6 @@
 <body onLoad="initMap()" style="margin:0px; border:0px; padding:0px;">
     <div id="map"></div>
 </body>
-
 </html>
 
 <?php get_footer(); ?>
